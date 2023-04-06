4a434f5107406cc5c9d61501afd2c84844a24743
Lee Grobler <hello@lee-grobler.com>
2023-04-06 13:24:10 +0200

    Added unit testing

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Weather from './components/Weather'
 const App = () => {
   return (
     <div className="container mx-auto px-4 py-8">
+      <h1 className="mb-8 uppercase text-gray-400">Weather App Pro</h1>
       <Weather />
     </div>
   )
